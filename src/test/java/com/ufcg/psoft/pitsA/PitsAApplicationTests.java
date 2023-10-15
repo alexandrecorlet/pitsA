72adb78c308d3e8d92a95405570468f07c15d2b1
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2023-10-15 11:14:20 -0300

    Initial commit

---
+++ src/test/java/com/ufcg/psoft/pitsA/PitsAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufcg.psoft.pitsA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitsAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
