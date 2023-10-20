735bd56e6d261e34072c489fcfe769117565384e
i-assis <israel.dynamics@gmail.com>
2023-10-19 23:19:49 -0400

    ADD: Business class.

---
+++ src/main/java/com/ufcg/psoft/pitsA/model/Business.java
@@ -0,0 +1,69 @@
+package com.ufcg.psoft.pitsA.model;
+
+import java.util.Objects;
+
+public class Business {
+
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    // TODO: deliverymanRegistrations Map
+
+    // TODO: customerRegistrations Map
+
+    public Business() {
+
+    };
+
+    public Business(String name, String password) {
+        this.name = name;
+        this.password = password;
+    };
+
+    public void setName(String name) {
+        this.name = name;
+    };
+
+    public void setPassword(String name) {
+        this.password = password;
+    };
+
+    public String getName() {
+        return name;
+    };
+
+    public String getPassword() {
+        return password;
+    };
+
+    public Long getId() {
+        return id;
+    };
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Business business)) {
+            return false;
+        } else if (this == o) {
+            return true;
+        };
+
+        return id.equals(business.getId());
+
+    };
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    };
+
+    @Override
+    public String toString() {
+        return "Name: " + name + '\n'
+                + "Id: " + id;
+    };
+
+}
