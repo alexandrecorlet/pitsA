28071f9c885b8a0e1c459122ff63062a91e40543
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2023-11-10 01:31:16 -0300

    add: DeliverymanNotFoundException class and InvalidPasswordException class.

---
+++ src/main/java/com/ufcg/psoft/pitsA/exception/DeliverymanNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ufcg.psoft.pitsA.exception;
+
+public class DeliverymanNotFoundException extends Exception {
+
+    public DeliverymanNotFoundException(String errorMessage) {
+        super(errorMessage);
+    }
+
+}
---
+++ src/main/java/com/ufcg/psoft/pitsA/exception/InvalidPasswordException.java
@@ -0,0 +1,9 @@
+package com.ufcg.psoft.pitsA.exception;
+
+public class InvalidPasswordException extends Exception {
+
+    public InvalidPasswordException(String errorMessage) {
+        super(errorMessage);
+    }
+
+}
